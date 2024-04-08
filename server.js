93b5ec532a3a408a7ce6a52f1acaea1c3295293a
La7rodectus <la7rodectus@gmail.com>
2024-04-08 22:21:34 +0300

    init

--- server.js
+++ server.js
@@ -1,32 +1,41 @@
 const express = require('express');
-const axios = require('axios');
 const app = express()
-const port = 8000
-const FluentClient = require('@fluent-org/logger').FluentClient;
+const port = 9091
+const Prometheus = require('prom-client')
 
-const logger = new FluentClient('fluentd.test', {
-    socket: {
-      host: 'localhost',
-      port: 8080,
-      timeout: 3000, // 3 seconds
-    }
-  });
+const metricsInterval = Prometheus.collectDefaultMetrics()
+const checkoutsTotal = new Prometheus.Counter({
+  name: 'checkouts_total',
+  help: 'Total number of checkouts',
+  labelNames: ['payment_method']
+})
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]  // buckets for response time from 0.1ms to 500ms
+})
 
-const link = 'https://openlibrary.org/search.json';
+app.get('/checkout', (req, res, next) => {
+  const paymentMethod = Math.round(Math.random()) === 0 ? 'stripe' : 'paypal'
 
-app.get('/', async (req, res) => {
-  const params = req.query;
-  console.log('Calling with parameters ', params);
-  logger.emit('Query params', {params});
-  const {data: {docs: [firstHit]}} = await axios.get(link, {params})
-  logger.emit('Response', {firstHit})
+  checkoutsTotal.inc({
+    payment_method: paymentMethod
+  })
 
-  res.send(firstHit)
+  res.json({ status: 'ok' })
+  next()
+})
+
+app.get('/metrics', async (req, res, next) => {
+
+  res.set('Content-Type', Prometheus.register.contentType)
+  res.end(await Prometheus.register.metrics())
+  next()
 });
 
 
 
 app.listen(port, () => {
-  logger.emit('App', {data: 'App started listening', port})
   console.log(`Example app listening on port ${port}`)
 })
